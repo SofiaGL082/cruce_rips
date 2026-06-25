b8925849cb406f3c3eef42b30be4b4db5c9f4a02
SofiaGL082 <141962316+SofiaGL082@users.noreply.github.com>
2026-06-25 04:03:49 -0500

    Orden para ejecutable

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
 from flask import Flask, render_template, request, send_file
 from pathlib import Path
 from procesamiento import crear_cruce
+import webbrowser
+import threading
+import os
+import sys
 
-app = Flask(__name__)
+def ruta_recurso(ruta_relativa):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, ruta_relativa)
+
+app = Flask(
+    __name__,
+    template_folder=ruta_recurso("templates"),
+    static_folder=ruta_recurso("static")
+    )
 
 @app.route("/", methods=["GET", "POST"])
 def inicio():
@@ -34,7 +50,9 @@ def inicio():
                 hoja=hoja_procesar
             )
 
-            app.config["ULTIMO_RESULTADO"] = resultado["archivo_salida"]
+            app.config["ULTIMO_RESULTADO"] = str(
+                Path(resultado["archivo_salida"]).resolve()
+            )
 
             ruta_archivo.unlink(missing_ok=True)
 
@@ -60,5 +78,9 @@ def descargar():
         download_name=ruta_archivo.name
     )
 
+def abrir_navegador():
+    webbrowser.open("http://127.0.0.1:5000")
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    threading.Timer(1, abrir_navegador).start()
+    app.run(debug=False)
